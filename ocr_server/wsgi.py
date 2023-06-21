9bd7517b4f864d95b68448c0a9ef08e6671a9f0f
kalvinbenser <kalvinbenser1@gmail.com>
2023-06-21 23:52:46 +0530

    versal deploy

--- ocr_server/wsgi.py
+++ ocr_server/wsgi.py
@@ -14,3 +14,6 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ocr_server.settings')
 
 application = get_wsgi_application()
+
+app = application
+
