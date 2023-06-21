3a300c144baa0f9b913eea9541cdb87912450833
kalvinbenser <kalvinbenser1@gmail.com>
2023-06-22 00:00:24 +0530

    allowed host

--- ocr_server/settings.py
+++ ocr_server/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-0gm!tpe-eq))ee08(u8#5i+d5_c^##yhqg-ybo70cm5@rsragz
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000"
